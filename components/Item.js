5fbadb1d0e8187fce587c381b44523a7c5c8f0bd
Travis Gorman <thefarbetween@me.com>
2016-12-08 17:30:42 -0600

    onEdit and onCancel event handlers

--- components/Item.js
+++ components/Item.js
@@ -1,15 +1,52 @@
 import React from 'react'
 
 export default class Item extends React.Component{
-  render(){
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      isEditing: false,
+    }
+  }
+
+renderActionsSection() {
+  if (this.state.isEditing) {
     return (  
-        <tr>
-          <td>{this.props.task}</td>
-          <td>
-            <button>Edit</button>
-            <button>Delete</button>
-          </td>
-        </tr>
+      <td>
+          <button>Save</button>
+          <button onClick={this.onCancelClick.bind(this)}>
+            Cancel
+          </button>
+      </td>
     );
   }
+  return (  
+    <td>
+        <button onClick={this.onEditClick.bind(this)}>Edit</button>
+        <button>Delete</button>  
+    </td>
+  )
+}
+
+render(){
+  return (  
+    <tr>
+        <td>{this.props.task}</td>
+        {this.renderActionsSection()}
+    </tr>
+  );
+}
+
+onEditClick(){
+  this.setState({
+    isEditing: true
+  });
+}
+
+onCancelClick(){
+  this.setState({
+    isEditing: false
+  })
+}
 }
