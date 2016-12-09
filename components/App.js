51561b96590fbf9cee364f9df4f392ae7cfa676e
Travis Gorman <thefarbetween@me.com>
2016-12-08 18:49:05 -0600

    input new todo, and clear field

--- components/App.js
+++ components/App.js
@@ -15,19 +15,33 @@ const todos = [
     task: 'Commit this code',
     isComplete: false,
   },
-]
+];
+
 export default class App extends React.Component{
+
   constructor(props) {
     super(props);
-    this.state = {todos}
+    this.state = {todos};
   }
+
   render() {
     return (  
       <div>
-        <h1>React ToDos App</h1>
-        <Create />
-        <List todos={this.state.todos} />
+          <h1>React ToDos App</h1>
+          <Create createTask={this.createTask.bind(this)} />
+          <List todos={this.state.todos} />
       </div>
     );
   }
+
+  createTask(task){
+    this.state.todos.push({
+      task, 
+      isComplete: false,
+    });
+    this.setState({ 
+      todos: this.state.todos 
+    });
+  }
+  
 }
