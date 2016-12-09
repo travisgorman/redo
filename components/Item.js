8303634ddc5bb234514c22da338ac9806e5c2d9d
Travis Gorman <thefarbetween@me.com>
2016-12-09 04:47:25 -0600

    all functionality works

--- components/Item.js
+++ components/Item.js
@@ -1,61 +1,77 @@
 import React from 'react'
 
 export default class Item extends React.Component{
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       isEditing: false,
-    }
+    };
   }
-renderTaskSection(){
-  const { task, isComplete } = this.props;
-  const taskStyle = {
-    color: isComplete ? 'green' : 'red',
-    cursor: 'pointer',
-  }
-  return (  
-    <td style={taskStyle}>{task}</td>
-  )
-}
 
-renderActionsSection() {
-  if (this.state.isEditing) {
-    return (  
-      <td>
-          <button>Save</button>
-          <button onClick={this.onCancelClick.bind(this)}>
-            Cancel
-          </button>
+  renderTaskSection() {
+    const { task, isComplete } = this.props;
+    const taskStyle = {
+      color: isComplete ? 'green' : 'red',
+      cursor: 'pointer',
+    }
+
+    if (this.state.isEditing) {
+      return (  
+        <td>
+          <form onSubmit={this.onSaveClick.bind(this)} />
+            <input type="text" defaultValue={task} ref="editInput" />
+        </td>
+      );
+    }
+
+
+    return (
+       <td style={taskStyle} 
+          onClick={this.props.toggleTask.bind(this, task)}>
+         {task}
       </td>
     );
   }
-  return (  
-    <td>
-        <button onClick={this.onEditClick.bind(this)}>Edit</button>
-        <button>Delete</button>  
-    </td>
-  )
-}
 
-render(){
-  return (  
-    <tr>
-        {this.renderTaskSection()}
-        {this.renderActionsSection()}
-    </tr>
-  );
-}
+  renderActionsSection() {
+    if (this.state.isEditing) {
+      return (
+        <td>
+            <button onClick={this.onSaveClick.bind(this)}>Save</button>
+            <button onClick={this.onCancelClick.bind(this)}>Cancel</button>
+        </td>
+      );
+    }
+    return (
+      <td>
+          <button onClick={this.onEditClick.bind(this)}>Edit</button>
+          <button onClick={this.props.deleteTask.bind(this, this.props.task)}>Delete</button>  
+      </td>
+    );
+  }
 
-onEditClick(){
-  this.setState({
-    isEditing: true
-  });
-}
+  render() {
+    return (
+      <tr>
+          {this.renderTaskSection()}
+          {this.renderActionsSection()}
+      </tr>
+    );
+  }
 
-onCancelClick(){
-  this.setState({
-    isEditing: false
-  })
-}
+  onEditClick() {
+    this.setState({ isEditing: true });
+  }
+
+  onCancelClick() {
+    this.setState({ isEditing: false });
+  }
+
+  onSaveClick(e) {
+    e.preventDefault();
+    const oldTask = this.props.task;
+    const newTask = this.refs.editInput.value;
+    this.props.saveTask(oldTask, newTask);
+    this.setState({ isEditing: false });
+  }
 }
--- components/List.js
+++ components/List.js
@@ -1,29 +1,22 @@
-import React from 'react'
-import Head from './Head'
+import React from 'react';
 import _ from 'lodash';
-import Item from './Item'
-
+import Head from './Head';
+import Item from './Item';
 
 export default class List extends React.Component{
-  renderItems(){
-    return _.map(this.props.todos, (todo, index) => 
-      <Item key={index}{...todo} />);
+  renderItems() {
+    const props = _.omit(this.props, 'todos');b
+    return _.map(this.props.todos, (todo, index) => <Item key={index}{...todo}{...props}/>);
   }
 
-  render(){
-    return (  
+  render() {
+    return (
       <table>
-        <Head />
-        <tbody>
-          {this.renderItems()}
-        </tbody>
+          <Head/>
+          <tbody>
+            {this.renderItems()}
+          </tbody>
       </table>
     );
-  }
-  handleCreate(e){
-    e.preventDefault()
-    this.props.createTask(this.refs.createInput.value);
-    this.refs.createInput.value = '';
-    console.log( this.refs.createInput.value )
   }
 }
